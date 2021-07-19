edb532924575d28eaa53899fad186cf098dde91f
Abdelkhalek Aberhouch <a.aberhouch@gmail.com>
2021-07-19 00:38:57 -0400

    chg describe()

--- src/explore.py
+++ src/explore.py
@@ -10,5 +10,5 @@ df = pd.read_csv('data/Ad.csv', index_col=0)
 # create folder to save file
 data_path = os.path.join('data', 'Exploration')
 os.makedirs(data_path, exist_ok=True)
-out = df.describe()[0:1]
+out = df.describe()
 out.to_csv(os.path.join(data_path, "nblignes.csv"))
